0757b91c14d638a2bf944f809375696db5d1064b
JOHN NABIL <mahatozy@gmail.com>
2023-03-26 22:27:11 +0200

    Initial commit

--- udagram/set_env.sh
+++ udagram/set_env.sh
@@ -8,4 +8,4 @@ export AWS_BUCKET=arn:aws:s3:::john0790-3674-9876
 export AWS_REGION=us-east-1
 export AWS_PROFILE=default
 export JWT_SECRET=mysecretstring
-export URL=http://localhost:8100
+export URL=http://john0790-3674-9876.s3-website-us-east-1.amazonaws.com
