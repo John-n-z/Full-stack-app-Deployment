5d2b56e27fa03a52bd6d5dde9b5d8bc3c76e7137
JOHN NABIL <mahatozy@gmail.com>
2023-03-26 21:20:15 +0200

    Initial commit

---
+++ udagram/udagram-frontend/bin/deploy.sh
@@ -0,0 +1,2 @@
+aws s3 cp --recursive --acl public-read ./www s3://john0790-3674-9876/
+aws s3 cp --acl public-read --cache-control="max-age=0, no-cache, no-store, must-revalidate" ./www/index.html s3://john0790-3674-9876/
