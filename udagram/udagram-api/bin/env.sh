24b1c9d49d91283cf3e9376b524f4df296acb0d0
JOHN NABIL <mahatozy@gmail.com>
2023-03-28 17:12:05 +0200

    Initial commit

---
+++ udagram/udagram-api/bin/env.sh
@@ -0,0 +1,10 @@
+eb setenv 
+POSTGRES_USERNAME=postgres 
+POSTGRES_PASSWORD=john5613231 
+POSTGRES_HOST=johndb.cyqj4yvkmnxi.us-east-1.rds.amazonaws.com 
+POSTGRES_DB=postgres 
+AWS_BUCKET=arn:aws:s3:::john0790-3674-9876 
+AWS_DEFAULT_REGION=us-east-1 
+AWS_PROFILE=default 
+JWT_SECRET=mysecretstring 
+URL=http://john0790-3674-9876.s3-website-us-east-1.amazonaws.com
